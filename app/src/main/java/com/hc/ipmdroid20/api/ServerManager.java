9f22d4a13585420aac81c5e83f7a621f0839a470
harvestcore <aagomezies@gmail.com>
2021-06-03 13:01:07 +0200

    Add ServerManager backbone. Closes #1

---
+++ app/src/main/java/com/hc/ipmdroid20/api/ServerManager.java
@@ -0,0 +1,38 @@
+package com.hc.ipmdroid20.api;
+
+import com.hc.ipmdroid20.api.models.Server;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class ServerManager {
+    private static ServerManager manager;
+    private HashMap<UUID, Server> servers;
+    private Server currentServer;
+
+    private ServerManager() {
+        servers = new HashMap<>();
+    }
+
+    public static ServerManager Instance() {
+        if (manager == null) {
+            manager = new ServerManager();
+        }
+
+        return manager;
+    }
+
+    public void loadServers() {
+
+    }
+
+    public void setCurrentServer(String uuid) {
+        if (uuid != null && uuid.equals("")) {
+            currentServer = servers.get(UUID.fromString(uuid));
+        }
+    }
+
+    public Server getCurrentServer() {
+        return currentServer;
+    }
+}
