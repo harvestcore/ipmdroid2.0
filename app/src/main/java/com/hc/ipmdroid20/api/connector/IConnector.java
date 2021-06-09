3c496192997ffee33bfb5768dbe7e00fa9b303a2
harvestcore <aagomezies@gmail.com>
2021-06-09 23:13:42 +0200

    Remove unused imports

--- app/src/main/java/com/hc/ipmdroid20/api/connector/IConnector.java
+++ app/src/main/java/com/hc/ipmdroid20/api/connector/IConnector.java
@@ -11,12 +11,10 @@ import com.hc.ipmdroid20.api.models.status.Status;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.HTTP;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
-import retrofit2.http.Path;
 
 public interface IConnector {
     // STATUS
